7b7ade47ed6ab33d656f2a36f8a8d85a4c5c7b0e
LobsterQ <616664218@qq.com>
2018-03-16 10:57:33 +0800

    22018.3.16-v2
    --解决了悬浮窗到主界面的bug

--- app/src/main/java/com/example/administrator/screenpet/FloatWindowBigView.java
+++ app/src/main/java/com/example/administrator/screenpet/FloatWindowBigView.java
@@ -48,6 +48,7 @@ public class FloatWindowBigView extends LinearLayout {
                 MyWindowManager.removeBigWindow(context);
                 MyWindowManager.createSmallWindow(context);
                 Intent intent = new Intent(getContext(), MainSettingActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent);
             }
         });
